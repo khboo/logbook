16f6d276484b06dfd1f3d5a5c6d066bfd039f440
kihupboo <kihupboo@kihups-mbp.cbc-rc.ca>
2017-12-15 09:18:23 -0500

    Initial code.

---
+++ src/main/java/com/borasoft/radio/logbook/adif/Header.java
@@ -0,0 +1,38 @@
+package com.borasoft.radio.logbook.adif;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+class Header {
+	String freetext; // header start. should not be starting with '<'. header ends with <eoh>.
+	String adif_ver;
+	String programid;
+	String programversion;
+	JSONObject toJSONObject() throws JSONException {
+		JSONObject record= new JSONObject();
+		addIfNotNull(record,"freetext",freetext);
+		addIfNotNull(record,"adif_ver",adif_ver);
+		addIfNotNull(record,"programid",programid);
+		addIfNotNull(record,"programversion",programversion);	
+		return record;
+	}
+	void addIfNotNull(JSONObject record,String field,String value) throws JSONException {
+		if(value!=null) {
+			record.put(field,value);
+		}
+	}
+	String toADIFString() {
+		StringBuilder record=new StringBuilder();
+		record.append(freetext);
+		addIfNotNull(record,"adif_ver",adif_ver);
+		addIfNotNull(record,"programid",programid);
+		addIfNotNull(record,"programversion",programversion);	
+		record.append("<eoh>");
+		return record.toString();
+	}
+	void addIfNotNull(StringBuilder record,String field,String value) {
+		if(value!=null) {
+			record.append("<"+field+":"+value.length()+">"+value);
+		}
+	}	
+}
