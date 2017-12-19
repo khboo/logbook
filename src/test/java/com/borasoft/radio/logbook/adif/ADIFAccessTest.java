c87c140daf5dc1a8de3c10339d49006a106fa486
kihupboo <kihupboo@kihups-mbp.cbc-rc.ca>
2017-12-19 15:55:16 -0500

    One more try for test failure.

--- src/test/java/com/borasoft/radio/logbook/adif/ADIFAccessTest.java
+++ src/test/java/com/borasoft/radio/logbook/adif/ADIFAccessTest.java
@@ -12,12 +12,11 @@ import org.junit.Test;
 public class ADIFAccessTest {
 	@Test
 	public void testADIFRead() throws URISyntaxException, IOException {
-		FileInputStream stream = new FileInputStream(new java.io.File(ADIFAccessTest.class.getClassLoader().getResource("ve3he_2017-12-09.ADI").toURI()));
+		FileInputStream stream = new FileInputStream(new java.io.File(getClass().getClassLoader().getResource("ve3he_2017-12-09.ADI").toURI()));
 		InputStreamReader reader = new InputStreamReader(stream);
 		ADIFReader adifReader = new ADIFReader(reader);
 		File adif= adifReader.readADIF_File();
 		reader.close();
-		System.out.println(adif.header.freetext.trim());
 		assertEquals(adif.header.freetext.trim(),"header <adif_ver:4>1.00 From VE6YP log programme.");
 		assertEquals(adif.header.adif_ver,"1.00");
 		assertEquals(adif.records.size(),9);
