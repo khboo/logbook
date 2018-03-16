cd91d3864127005e4c7bf0472fd3f1a50ab1f260
kihupboo <kihupboo@kihups-mbp.cbc-rc.ca>
2018-03-16 09:02:42 -0400

    Added test for JSON outputs

--- src/test/java/com/borasoft/radio/logbook/adif/ADIF_FileTest.java
+++ src/test/java/com/borasoft/radio/logbook/adif/ADIF_FileTest.java
@@ -2,6 +2,7 @@ package com.borasoft.radio.logbook.adif;
 
 import static org.junit.Assert.assertEquals;
 
+import org.json.JSONException;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -10,6 +11,9 @@ public class ADIF_FileTest {
 			"Hello header<adif_ver:3>1.0<programid:4>1234<programversion:3>1.0<eoh>\n"
 			+"<call:6>va3pen<qso_date:8>20171130<time_on:4>1300<band:3>20m<mode:3>SSB<eor>\n"
 			+"<call:5>ve3he<qso_date:8>20171215<time_on:4>1300<band:3>40m<mode:3>SSB<eor>\n";
+	final String expectedHeaderJSON="{\"freetext\":\"Hello header\",\"programversion\":\"1.0\",\"adif_ver\":\"1.0\",\"programid\":\"1234\"}";
+	final String expectedRecordsJSON="[{\"call\":\"va3pen\",\"mode\":\"SSB\",\"qso_date\":\"20171130\",\"band\":\"20m\",\"time_on\":\"1300\"},"
+			+ "{\"call\":\"ve3he\",\"mode\":\"SSB\",\"qso_date\":\"20171215\",\"band\":\"40m\",\"time_on\":\"1300\"}]";
 	File file=new File();
 	@Before
 	public void setup() {
@@ -28,5 +32,12 @@ public class ADIF_FileTest {
 	public void testADIFOutput() {
 		System.out.println(file.toADIFString());
 		assertEquals("ADIF output does not match the expected value.",expectedADIF,file.toADIFString());
+	}
+	@Test
+	public void testJSONOutput() throws JSONException {
+		System.out.println(file.toJSONObject().getString("header"));
+		assertEquals("Header JSON output does not match the expected value.",expectedHeaderJSON,file.toJSONObject().getString("header"));
+		System.out.println(file.toJSONObject().optJSONArray("records").toString());
+		assertEquals("Records JSON output does not match the expected value.",expectedRecordsJSON,file.toJSONObject().optJSONArray("records").toString());
 	}
 }
